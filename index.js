ec2a6f063b4e93b1073f6932d136036a6d6e1dcf
Malcom X <Taajlogan@gmail.com>
2024-04-30 19:49:28 -0400

    addedSvgClass

--- index.js
+++ index.js
@@ -3,3 +3,22 @@ import inquirer from 'inquirer';
 import fs from 'graceful-fs'
 import {circle, square, trianlge} from './lib/shapes.js';
 
+class Svg {
+    constructor() {
+        const {textElement = '' , shapeElement = ''} = {};
+        this.textElement = textElement;
+        this.shapeElement = shapeElement;
+    }
+
+    render(){
+        return ``
+    }
+
+    setTextElemt(text = '' , color = '') {
+        this.textElement = ``
+    }
+
+    setShapeElement(shape) {
+        this.shapeElement = shape.render();
+    } 
+}
