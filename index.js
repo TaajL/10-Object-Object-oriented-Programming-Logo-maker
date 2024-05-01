b15011e5f3bbed6d08b205a55cdfdd404a53b73f
Malcom X <Taajlogan@gmail.com>
2024-04-30 20:39:06 -0400

    addedSVGclass

--- index.js
+++ index.js
@@ -2,23 +2,53 @@
 import inquirer from 'inquirer';
 import fs from 'graceful-fs'
 import {circle, square, trianlge} from './lib/shapes.js';
+import { choices } from 'yargs';
 
+//Defines and Svg Class that represents and SVG element
 class Svg {
     constructor() {
         const {textElement = '' , shapeElement = ''} = {};
         this.textElement = textElement;
         this.shapeElement = shapeElement;
     }
-
+    // Render the SVG element as string 
     render(){
         return ``
     }
-
+    // Set the text element  with the given text and color
     setTextElemt(text = '' , color = '') {
         this.textElement = ``
     }
-
+    // set the shape element using the given shape object
     setShapeElement(shape) {
         this.shapeElement = shape.render();
     } 
 }
+
+const questions = [
+    {
+        type: 'input',
+        name: 'text',
+        message: `TEXT: Enter up to (3) Characters:`,
+    },
+    {
+        type: 'input',
+        name: 'text-color',
+        message: `TEXT COLOR: Enter a color keyword (OR a hexadecimal number):`, 
+    },
+    {
+        type: 'input',
+        name: 'shape-color',
+        message: `SHAPE COLOR: Enter a color keyword (OR a hexadecimal number):`,
+    },
+    {
+        type: 'List',
+        name: 'pixel-image',
+        message: 'Choose which Pixel Image you would like?',
+        choices: ['Circle', 'Square', 'Triangle'],
+    },
+];
+
+const { text, text_color, shape_color pixel_image } = await inquirer.prompt
+
+
