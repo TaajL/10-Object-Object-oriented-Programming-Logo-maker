7ee9da36ca7454b4cfec0b9056a0093cae8511cc
Malcom X <Taajlogan@gmail.com>
2024-05-07 11:15:11 -0400

    addrendertoSCGfnct

--- index.js
+++ index.js
@@ -101,18 +101,20 @@ async function init() {
 
     // create a new SVG element adding text and shape elements to it 
     const svg = new class {
-        constructor (){
+        constructor() {
             this.textElement = `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${userFontColor}">${userText}</text>`;
-            this.shapeElement = userShape.render()
+            this.shapeElement = userShape.render();
         }
 
         render () {
-            return
-        }
+            return `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="300" height="200">${this.shapeElement}${this.textElement}</svg>`;
+                }
+        }();
+
     }
 
 
 
-}
+
 
 
